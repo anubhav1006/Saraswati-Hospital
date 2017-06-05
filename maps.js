94299f48d6acd82d4a0b50615949a6267fb34a97
PriyamShah <priyam.95shah@gmail.com>
2017-06-05 23:38:02 +0530

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,8 +1,8 @@
 
       function initMap() {
-        var place = {lat:28.6306933, lng: 76.998944};
+        var place = {lat:28.6306933, lng:76.992378};
         var map = new google.maps.Map(document.getElementById('map'), {
-           zoom: 11,
+           zoom: 15,
           center: place
         });
       }
