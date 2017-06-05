978ec0f66c7e8f9b46f826bd87010c42df4d0782
PriyamShah <priyam.95shah@gmail.com>
2017-06-06 00:02:53 +0530

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,6 +1,7 @@
 
       function initMap() {
         var place = {lat:28.6306933, lng:76.992378};
+            var marker= new google.maps.Marker({position:place,map:map});
         var map = new google.maps.Map(document.getElementById('map'), {
            zoom: 15,
           center: place
