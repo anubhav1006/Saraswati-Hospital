2043044e485f870a3b150a6f91d7ba89385dfefa
Anubhav Jha <aj1006.aj@gmail.com>
2017-06-05 13:34:04 +0530

    Map

---
+++ maps.js
@@ -0,0 +1,8 @@
+
+      function initMap() {
+        var place = {lat:20.5937, lng: 78.9629};
+        var map = new google.maps.Map(document.getElementById('map'), {
+           zoom: 4,
+          center: place
+        });
+      }
